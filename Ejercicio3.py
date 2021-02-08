4196620288d2f9045112d5c4a204f32d193834a9
cristopxer <cristopherortiz457@gmail.com>
2021-02-07 23:33:07 -0600

    Finished

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,11 +1,11 @@
 entries = int(input())
 while (entries > 0):
-    n = int(input())
-    for i in range(1, n+1):
-        if(i * (i+1) >= 2*n):
+    point = int(input())
+    for i in range(1, point+1):
+        if(i * (i+1) >= 2*point):
             break
-    k = ((i * (i+1))/2)
-    if(k == n+1):
+    currentPoint = ((i * (i+1))/2)
+    if(currentPoint == point+1):
         i += 1
     print(i)
     entries -= 1
--- Ejercicio3UsingFile.py
+++ Ejercicio3UsingFile.py
@@ -1,12 +1,12 @@
 f = open('Input.txt')
 entries = int(f.readline())
 while (entries > 0):
-    n = int(f.readline())
-    for i in range(1, n+1):
-        if(i * (i+1) >= 2*n):
+    point = int(f.readline())
+    for i in range(1, point+1):
+        if(i * (i+1) >= 2*point):
             break
-    k = ((i * (i+1))/2)
-    if(k == n+1):
+    currentPoint = ((i * (i+1))/2)
+    if(currentPoint == point+1):
         i += 1
-    print(f'for input: {n} Number of Jumps = {i}')
+    print(f'for input: {point} Number of Jumps = {i}')
     entries -= 1
