38fca0901516abec5acfb1f67300d089db7c72ed
cristopxer <cristopherortiz457@gmail.com>
2021-02-07 23:28:08 -0600

    Non file approach

--- Ejercicio3UsingFile.py
+++ Ejercicio3UsingFile.py
@@ -1,11 +1,12 @@
-entries = int(input())
+f = open('Input.txt')
+entries = int(f.readline())
 while (entries > 0):
-    n = int(input())
+    n = int(f.readline())
     for i in range(1, n+1):
         if(i * (i+1) >= 2*n):
             break
     k = ((i * (i+1))/2)
     if(k == n+1):
         i += 1
-    print(i)
+    print(f'for input: {n} Number of Jumps = {i}')
     entries -= 1
